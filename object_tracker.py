e13eb0e8e990ead2be06ba0f0254c57d489e7cd8
joy-yeah-jump <a717942@gmail.com>
2021-10-28 17:30:11 +0900

    tracking data class coding(about 50%) opening movie phase added intro phase added tagger sound added

--- object_tracker.py
+++ object_tracker.py
@@ -27,6 +27,11 @@ from deep_sort.detection import Detection
 from deep_sort.tracker import Tracker
 from tools import generate_detections as gdet
 
+from moviepy.editor import *
+import pygame
+# pygame.mixer.pre_init(44100, 16, 2, 4096)
+pygame.init()
+
 flags.DEFINE_boolean('count', False, 'count objects being tracked on screen')
 flags.DEFINE_boolean('dont_show', False, 'dont show video output')
 flags.DEFINE_string('framework', 'tf', 'tf, tflite, trt')
@@ -36,50 +41,18 @@ flags.DEFINE_string('model', 'yolov4', 'yolov3 or yolov4')
 flags.DEFINE_string('output', None, 'path to output video')
 flags.DEFINE_string('output_format', 'XVID', 'codec used in VideoWriter when saving video to file')
 flags.DEFINE_float('score', 0.50, 'score threshold')
-flags.DEFINE_integer('size', 416, 'resize images to')
+# flags.DEFINE_integer('size', 416, 'resize images to')
 # flags.DEFINE_boolean('tiny', False, 'yolo or yolo-tiny')
 flags.DEFINE_boolean('tiny', True, 'yolo or yolo-tiny')
 # flags.DEFINE_string('video', './data/video/test.mp4', 'path to input video or set to 0 for webcam')
-flags.DEFINE_string('video', '0', 'path to input video or set to 0 for webcam')
+# flags.DEFINE_string('video', '0', 'path to input video or set to 0 for webcam')
 flags.DEFINE_string('weights', './checkpoints/yolov4-tiny-416', 'path to weights file')
 
-
-
-def main(_argv):
-    # Definition of the parameters
-    max_cosine_distance = 0.4
-    nn_budget = None
-    nms_max_overlap = 1.0
-    
-    # initialize deep sort
-    model_filename = 'model_data/mars-small128.pb'
-    encoder = gdet.create_box_encoder(model_filename, batch_size=1)
-    # calculate cosine distance metric
-    metric = nn_matching.NearestNeighborDistanceMetric("cosine", max_cosine_distance, nn_budget)
-    # initialize tracker
-    tracker = Tracker(metric)
-
-    # load configuration for object detector
-    config = ConfigProto()
-    config.gpu_options.allow_growth = True
-    session = InteractiveSession(config=config)
-    STRIDES, ANCHORS, NUM_CLASS, XYSCALE = utils.load_config(FLAGS)
-    input_size = FLAGS.size
-    video_path = FLAGS.video
-
-    # load model
-    saved_model_loaded = tf.saved_model.load(FLAGS.weights, tags=[tag_constants.SERVING])
-    infer = saved_model_loaded.signatures['serving_default']
-
-    # begin video capture
-    vid = cv2.VideoCapture(int(video_path))
-
-    out = None
-
+def person_track(vid, tracker, infer, encoder) :
     is_1st = False
     coord_1st = None
     frame_num = 0
-    # while video is running
+    cam_open_time = time.time()
     while True:
         return_value, frame = vid.read()
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
@@ -87,7 +60,7 @@ def main(_argv):
         frame_num += 1
         # print('Frame #: ', frame_num)
         frame_size = frame.shape[:2]
-        image_data = cv2.resize(frame, (input_size, input_size))
+        image_data = cv2.resize(frame, (416, 416)) # 416 = input_size
         image_data = image_data / 255.
         image_data = image_data[np.newaxis, ...].astype(np.float32)
         start_time = time.time()
@@ -149,9 +122,10 @@ def main(_argv):
 
         # encode yolo detections and feed to tracker
         features = encoder(frame, bboxes)
-        detections = [Detection(bbox, score, class_name, feature) for bbox, score, class_name, feature in zip(bboxes, scores, names, features)]
+        detections = [Detection(bbox, score, class_name, feature) for bbox, score, class_name, feature in
+                      zip(bboxes, scores, names, features)]
 
-        #initialize color map
+        # initialize color map
         cmap = plt.get_cmap('tab20b')
         colors = [cmap(i)[:3] for i in np.linspace(0, 1, 20)]
 
@@ -159,8 +133,8 @@ def main(_argv):
         boxs = np.array([d.tlwh for d in detections])
         scores = np.array([d.confidence for d in detections])
         classes = np.array([d.class_name for d in detections])
-        indices = preprocessing.non_max_suppression(boxs, classes, nms_max_overlap, scores)
-        detections = [detections[i] for i in indices]       
+        indices = preprocessing.non_max_suppression(boxs, classes, 1.0, scores) # 1.0 = nms_max_overlap
+        detections = [detections[i] for i in indices]
 
         # Call the tracker
         tracker.predict()
@@ -170,7 +144,7 @@ def main(_argv):
         # update tracks
         for track in tracker.tracks:
             if not track.is_confirmed() or track.time_since_update > 1:
-                continue 
+                continue
             bbox = track.to_tlbr()
             class_name = track.get_class()
 
@@ -178,10 +152,12 @@ def main(_argv):
             color = colors[int(track.track_id) % len(colors)]
             color = [i * 255 for i in color]
             cv2.rectangle(frame, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), color, 2)
-            cv2.rectangle(frame, (int(bbox[0]), int(bbox[1]-30)), (int(bbox[0])+(len(class_name)+len(str(track.track_id)))*8, int(bbox[1])), color, -1)
-            cv2.putText(frame, "id-" + str(track.track_id), (int(bbox[0]), int(bbox[1]-10)), 0, 0.75, (255,255,255),2)
+            cv2.rectangle(frame, (int(bbox[0]), int(bbox[1] - 30)),
+                          (int(bbox[0]) + (len(class_name) + len(str(track.track_id))) * 8, int(bbox[1])), color, -1)
+            cv2.putText(frame, "id-" + str(track.track_id), (int(bbox[0]), int(bbox[1] - 10)), 0, 0.75, (255, 255, 255),
+                        2)
 
-            #red target circle
+            # red target circle
             center = (int((bbox[0] + bbox[2]) / 2), int((bbox[1] + bbox[3]) / 2))
             red = (255, 0, 0)
             cv2.circle(frame, center, 20, red, 2)
@@ -191,40 +167,171 @@ def main(_argv):
             cv2.line(frame, (center[0], center[1] - 15), (center[0], center[1] - 40), red, 2)
             cv2.line(frame, (center[0], center[1] + 15), (center[0], center[1] + 40), red, 2)
 
-            if not is_1st : # id별로 잡을것
+            if not is_1st:  # id별로 잡을것
                 coord_1st = bbox[:]
                 is_1st = True
                 print(coord_1st)
 
             # print(track.track_id) # id
             # print(track.to_tlbr()) # coords
-            cv2.rectangle(frame, (int(coord_1st[0]), int(coord_1st[1])), (int(coord_1st[2]), int(coord_1st[3])), color, 2)
-            cv2.rectangle(frame, (int(coord_1st[0]), int(coord_1st[1] - 30)), (int(coord_1st[0]) + (len(class_name) + len(str(track.track_id)))*12, int(coord_1st[1])), color, -1)
-            cv2.putText(frame, str(track.track_id) + '-first', (int(coord_1st[0]), int(coord_1st[1]-10)), 0, 0.75, (255, 255, 255), 2)
+            cv2.rectangle(frame, (int(coord_1st[0]), int(coord_1st[1])), (int(coord_1st[2]), int(coord_1st[3])), color,
+                          2)
+            cv2.rectangle(frame, (int(coord_1st[0]), int(coord_1st[1] - 30)),
+                          (int(coord_1st[0]) + (len(class_name) + len(str(track.track_id))) * 12, int(coord_1st[1])),
+                          color, -1)
+            cv2.putText(frame, str(track.track_id) + '-first', (int(coord_1st[0]), int(coord_1st[1] - 10)), 0, 0.75,
+                        (255, 255, 255), 2)
             coord_now = bbox[:]
             now_area = (coord_now[2] - coord_now[0]) * (coord_now[3] - coord_now[1])
             inter_area = 0
-            inter = [max(coord_1st[0], coord_now[0]), max(coord_1st[1], coord_now[1]), min(coord_1st[2], coord_now[2]), min(coord_1st[3], coord_now[3])]
-            if inter[0] < inter[2] and inter[1] < inter[3] :
+            inter = [max(coord_1st[0], coord_now[0]), max(coord_1st[1], coord_now[1]), min(coord_1st[2], coord_now[2]),
+                     min(coord_1st[3], coord_now[3])]
+            if inter[0] < inter[2] and inter[1] < inter[3]:
                 inter_area = (inter[2] - inter[0]) * (inter[3] - inter[1])
 
         # calculate frames per second of running detections
         fps = 1.0 / (time.time() - start_time)
-        if frame_num % 10 == 0 :
+        if frame_num % 10 == 0:
             try:
-                print('Frame # : {:>4} | FPS : {:>5.2f} | {:>6.2f}%'.format(frame_num, fps, inter_area / now_area * 100))
-            except :
+                print(
+                    'Frame # : {:>4} | FPS : {:>5.2f} | {:>6.2f}% | {}sec'.format(frame_num, fps, inter_area / now_area * 100, time.time() - cam_open_time))
+            except:
                 print("nothing detected or error occured")
         result = np.asarray(frame)
         result = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-        
-        cv2.imshow("Output Video", result)
-        if cv2.waitKey(1) & 0xFF == ord('q'): break
 
+        cv2.imshow("detecting window", result)
+        # cv2.imshow("detecting window", result) # no webcam window
+        if cv2.waitKey(1) & 0xFF == ord('q'): break
+        for event in pygame.event.get() :
+            if event.type == pygame.QUIT : break
+
+def main(_argv):
+    # Definition of the parameters
+    max_cosine_distance = 0.4
+    nn_budget = None
+    nms_max_overlap = 1.0
+    
+    # initialize deep sort
+    model_filename = 'model_data/mars-small128.pb'
+    encoder = gdet.create_box_encoder(model_filename, batch_size=1)
+    # calculate cosine distance metric
+    metric = nn_matching.NearestNeighborDistanceMetric("cosine", max_cosine_distance, nn_budget)
+    # initialize tracker
+    tracker = Tracker(metric)
+
+    # load configuration for object detector
+    config = ConfigProto()
+    config.gpu_options.allow_growth = True
+    session = InteractiveSession(config=config)
+    STRIDES, ANCHORS, NUM_CLASS, XYSCALE = utils.load_config(FLAGS)
+    # input_size = FLAGS.size #416
+    # video_path = FLAGS.video
+
+    # load model
+    saved_model_loaded = tf.saved_model.load(FLAGS.weights, tags=[tag_constants.SERVING])
+    infer = saved_model_loaded.signatures['serving_default']
+    # out = None
+
+    vid = cv2.VideoCapture(0) # 0 = int(video_path) = webcam
+
+    pygame.display.set_caption("tagger saying")
+    # cannot play gif. show stopped image
+    pygame.display.set_mode((640, 360)).blit(pygame.transform.scale(pygame.image.load("see_back.gif"), (640, 360)), [0, 0])
+    pygame.display.update()
+    pygame.mixer.music.load(os.path.dirname(os.path.abspath(__file__)) + '\\ko.mp3')
+    pygame.mixer.music.play()
+
+    # begin video capture
+    person_track(vid, tracker, infer, encoder) # while video is running
     cv2.destroyAllWindows()
+    pygame.quit()
+
+    # print('wait 5 sec')
+    # time.sleep(5)
+    # vid = cv2.VideoCapture(0)
+    # person_track(vid, tracker, infer, encoder)
+    # cv2.destroyAllWindows()
 
 if __name__ == '__main__':
     try:
-        app.run(main)
+        pygame.display.set_caption('opening movie')
+        clip = VideoFileClip('./data/video/cars.mp4')
+        clip.resize((640, 360)).preview()
+
+        pygame.display.set_caption('init screen')
+        screen = pygame.display.set_mode((640, 360))
+        screen.fill([0, 0, 0]) #[0, 0, 0] = black
+        bg = pygame.transform.scale(pygame.image.load("init_image.png"), (640, 360))
+        screen.blit(bg, [0, 0])
+        pygame.display.flip()
+
+        intro = True
+
+        while intro :
+            for event in pygame.event.get() :
+                if event.type == pygame.QUIT :
+                    intro = False
+                    break;
+
+            TextSurf = pygame.font.Font('freesansbold.ttf', 60).render("SQUID GAME", True, [0, 0, 0])
+            TextRect = TextSurf.get_rect()
+
+            TextRect.center = ((640/2), 30) # width = 640, height = 360
+            screen.blit(TextSurf, TextRect)
+
+            # left button
+            mouse = pygame.mouse.get_pos()
+            click = pygame.mouse.get_pressed()
+
+            # w = width, h = height, c = coordinate, l = length
+            b1_wc, b1_hc, b1_wl, b1_hl = 100, 310, 100, 50
+            if b1_wc+b1_wl > mouse[0] > b1_wc and b1_hc+b1_hl > mouse[1] > b1_hc :
+                pygame.draw.rect(screen, [127, 255, 127], (b1_wc, b1_hc, b1_wl, b1_hl))
+                if click[0] :
+                    screen.fill([0, 0, 0])
+
+                    # right bottom text : now loading...
+                    b3_wc, b3_hc, b3_wl, b3_hl = 450, 310, 100, 50
+
+                    b3Surf = pygame.font.Font('freesansbold.ttf', 20).render("NOW LOADING...", True, [255, 255, 255])
+                    b3Rect = b3Surf.get_rect()
+
+                    b3Rect.center = ((b3_wc + (b3_wl/2)), (b3_hc + (b3_hl/2)))
+                    screen.blit(b3Surf, b3Rect)
+
+                    pygame.display.update()
+                    break
+            else :
+                pygame.draw.rect(screen, [0, 255, 0], (b1_wc, b1_hc, b1_wl, b1_hl))
+
+            b1Surf = pygame.font.Font('freesansbold.ttf', 20).render("GO!", True, [0, 0, 0])
+            b1Rect = b1Surf.get_rect()
+
+            b1Rect.center = ((b1_wc+(b1_wl/2)), (b1_hc+(b1_hl/2)))
+            screen.blit(b1Surf, b1Rect)
+
+
+            # right button
+            b2_wc, b2_hc, b2_wl, b2_hl = 450, 310, 100, 50
+            pygame.draw.rect(screen, [255, 0, 0], (b2_wc, b2_hc, b2_wl, b2_hl))
+
+            b2Surf = pygame.font.Font('freesansbold.ttf', 20).render("QUIT", True, [0, 0, 0])
+            b2Rect = b2Surf.get_rect()
+
+            b2Rect.center = ((b2_wc+(b2_wl/2)), (b2_hc+(b2_hl/2)))
+            screen.blit(b2Surf, b2Rect)
+
+            if b2_wc+b2_wl > mouse[0] > b2_wc and b2_hc+b2_hl > mouse[1] > b2_hc :
+                if click[0] :
+                    intro = False
+                    break;
+
+            pygame.display.update()
+
+        if not intro :
+            pygame.quit()
+            quit()
+        else : app.run(main)
     except SystemExit:
         pass
--- person_track_id.py
+++ person_track_id.py
@@ -1,32 +1,33 @@
 class PersonTrackId :
-    id = -1
-    ix1, iy1, ix2, iy2 = -1      # first coordinate
-    lx1, ly1, lx2, ly2 = -1      # last coordinate
-    inx1, iny1, inx2, iny2 = -1  # intersection point
-    now = -1                     # now(last) area
-    inter = -1                   # intersection area
-
-    def __init__(self, id, ix1, iy1, ix2, iy2):
-        self.id = id
-        self.ix1 = ix1
-        self.iy1 = iy1
-        self.ix2 = ix2
-        self.iy2 = iy2
-
-    def set_coord(self, id, lx1, ly1, lx2, ly2):
-        if id != self.id :
-            print("id not same")
-            pass
-        self.lx1 = lx1
-        self.ly1 = ly1
-        self.lx2 = lx2
-        self.ly2 = ly2
-
-        self.inx1 = max(self.ix1, self.lx1)
-        self.iny1 = max(self.iy1, self.ly1)
-        self.inx2 = min(self.ix2, self.lx2)
-        self.iny2 = min(self.iy2, self.ly2)
-
-        if self.inx1 < self.inx2 and self.iny1 < self.iny2 :
-            self.inter = (self.inx2 - self.inx1) * (self.iny2 - self.iny1)
-        else : self.inter = -1
+    # id = -1
+    # ix1, iy1, ix2, iy2 = -1      # first coordinate
+    # lx1, ly1, lx2, ly2 = -1      # last coordinate
+    # inx1, iny1, inx2, iny2 = -1  # intersection point
+    # now = -1                     # now(last) area
+    # inter = -1                   # intersection area
+    #
+    # def __init__(self, id, ix1, iy1, ix2, iy2):
+    #     self.id = id
+    #     self.ix1 = ix1
+    #     self.iy1 = iy1
+    #     self.ix2 = ix2
+    #     self.iy2 = iy2
+    #
+    # def set_coord(self, id, lx1, ly1, lx2, ly2):
+    #     if id != self.id :
+    #         print("id not same")
+    #         pass
+    #     self.lx1 = lx1
+    #     self.ly1 = ly1
+    #     self.lx2 = lx2
+    #     self.ly2 = ly2
+    #
+    #     self.inx1 = max(self.ix1, self.lx1)
+    #     self.iny1 = max(self.iy1, self.ly1)
+    #     self.inx2 = min(self.ix2, self.lx2)
+    #     self.iny2 = min(self.iy2, self.ly2)
+    #
+    #     if self.inx1 < self.inx2 and self.iny1 < self.iny2 :
+    #         self.inter = (self.inx2 - self.inx1) * (self.iny2 - self.iny1)
+    #     else : self.inter = -1
+    pass
