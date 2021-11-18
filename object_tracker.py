a300b7c20be8ea5f632d730610f7b0ce3dbcd171
joy-yeah-jump <a717942@gmail.com>
2021-11-18 11:53:55 +0900

    id auth phase added timer, alive count added win/defect condition added result phase added(10%)

--- object_tracker.py
+++ object_tracker.py
@@ -2,6 +2,8 @@ import os
 # comment out below line to enable tensorflow logging outputs
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+# os.environ['IMAGEIO_FFMPEG_EXE'] = './ffmpeg-4.4.1-essentials_build/bin/ffmpeg.exe' # 맞아 이거 시발?
+os.environ['IMAGEIO_FFMPEG_EXE'] = './ffmpeg.exe'
 import time
 import tensorflow as tf
 
@@ -33,6 +35,8 @@ import pygame
 pygame.mixer.pre_init(44100, 16, 2, 4096)
 pygame.init()
 import threading
+from gtts import gTTS
+import random
 
 flags.DEFINE_boolean('count', False, 'count objects being tracked on screen')
 flags.DEFINE_boolean('dont_show', False, 'dont show video output')
@@ -75,7 +79,7 @@ def intro_screen() :
     pygame.display.set_caption('init screen')
     screen = pygame.display.set_mode((1920, 1080))
     screen.fill([0, 0, 0])  # [0, 0, 0] = black
-    bg = pygame.transform.scale(pygame.image.load("pic3.png"), (1920, 1080))
+    bg = pygame.transform.scale(pygame.image.load("intro.jpg"), (1920, 1080))
     screen.blit(bg, [0, 0])
     pygame.display.flip()
 
@@ -85,50 +89,51 @@ def intro_screen() :
                 pygame.quit()
                 sys.exit()
 
-        TextSurf = pygame.font.Font('freesansbold.ttf', 180).render("SQUID GAME", True, [255, 0, 0])
-        TextRect = TextSurf.get_rect()
-
-        TextRect.center = (1920 / 2, 180 / 2)  # 1920 = width, 180 = font size
-        screen.blit(TextSurf, TextRect)
+        # TextSurf = pygame.font.Font('freesansbold.ttf', 180).render("SQUID GAME", True, [255, 0, 0])
+        # TextRect = TextSurf.get_rect()
+        #
+        # TextRect.center = (1920 / 2, 180 / 2)  # 1920 = width, 180 = font size
+        # screen.blit(TextSurf, TextRect)
 
         # left button
         mouse = pygame.mouse.get_pos()
         click = pygame.mouse.get_pressed()
 
         # w = width, h = height, c = coordinate, l = length
-        b1_wc, b1_hc, b1_wl, b1_hl = 600, 450, 100, 50
+        b1_wc, b1_hc, b1_wl, b1_hl = 497, 456, 128, 83
         if b1_wc + b1_wl > mouse[0] > b1_wc and b1_hc + b1_hl > mouse[1] > b1_hc:
-            pygame.draw.rect(screen, [127, 255, 127], (b1_wc, b1_hc, b1_wl, b1_hl))
+            # pygame.draw.rect(screen, [127, 255, 127], (b1_wc, b1_hc, b1_wl, b1_hl))
             if click[0]:
                 screen.fill([0, 0, 0])
-                screen.blit(pygame.transform.scale(pygame.image.load('pic1.png'), (1920, 1080)), [0, 0])
+                screen.blit(pygame.transform.scale(pygame.image.load('loading.png'), (1920, 1080)), [0, 0])
 
-                b3Surf = pygame.font.Font('freesansbold.ttf', 40).render("NOW LOADING...", True, [0, 255, 255])
-                b3Rect = b3Surf.get_rect()
-
-                b3Rect.center = (1920 - 180, 1080 - 20)
-                screen.blit(b3Surf, b3Rect)
+                # b3Surf = pygame.font.Font('freesansbold.ttf', 40).render("NOW LOADING...", True, [0, 255, 255])
+                # b3Rect = b3Surf.get_rect()
+                #
+                # b3Rect.center = (1920 - 180, 1080 - 20)
+                # screen.blit(b3Surf, b3Rect)
 
                 pygame.display.update()
                 break
         else:
-            pygame.draw.rect(screen, [0, 255, 0], (b1_wc, b1_hc, b1_wl, b1_hl))
+            # pygame.draw.rect(screen, [0, 255, 0], (b1_wc, b1_hc, b1_wl, b1_hl))
+            pass
 
-        b1Surf = pygame.font.Font('freesansbold.ttf', 20).render("GO!", True, [0, 0, 0])
-        b1Rect = b1Surf.get_rect()
-
-        b1Rect.center = ((b1_wc + (b1_wl / 2)), (b1_hc + (b1_hl / 2)))
-        screen.blit(b1Surf, b1Rect)
+        # b1Surf = pygame.font.Font('freesansbold.ttf', 20).render("GO!", True, [0, 0, 0])
+        # b1Rect = b1Surf.get_rect()
+        #
+        # b1Rect.center = ((b1_wc + (b1_wl / 2)), (b1_hc + (b1_hl / 2)))
+        # screen.blit(b1Surf, b1Rect)
 
         # right button
-        b2_wc, b2_hc, b2_wl, b2_hl = 1200, 450, 100, 50
-        pygame.draw.rect(screen, [255, 0, 0], (b2_wc, b2_hc, b2_wl, b2_hl))
-
-        b2Surf = pygame.font.Font('freesansbold.ttf', 20).render("QUIT", True, [0, 0, 0])
-        b2Rect = b2Surf.get_rect()
-
-        b2Rect.center = ((b2_wc + (b2_wl / 2)), (b2_hc + (b2_hl / 2)))
-        screen.blit(b2Surf, b2Rect)
+        b2_wc, b2_hc, b2_wl, b2_hl = 1296, 454, 130, 83
+        # pygame.draw.rect(screen, [255, 0, 0], (b2_wc, b2_hc, b2_wl, b2_hl))
+        #
+        # b2Surf = pygame.font.Font('freesansbold.ttf', 20).render("QUIT", True, [0, 0, 0])
+        # b2Rect = b2Surf.get_rect()
+        #
+        # b2Rect.center = ((b2_wc + (b2_wl / 2)), (b2_hc + (b2_hl / 2)))
+        # screen.blit(b2Surf, b2Rect)
 
         if b2_wc + b2_wl > mouse[0] > b2_wc and b2_hc + b2_hl > mouse[1] > b2_hc:
             if click[0]:
@@ -137,16 +142,40 @@ def intro_screen() :
 
         pygame.display.update()
 
-def play_tagger_voice() :
-    pygame.mixer.music.load('ko.mp3')
+def play_tagger_voice(rint) :
+    f = 'voice' + str(rint) + '.mp3'
+    pygame.mixer.music.load(f)
     pygame.mixer.music.play()
 
 def get_go_forward_time() :
-    threading.Thread(target=play_tagger_voice()).start()
-    return 3
+    seed = [2.5, 3.5, 1.5] # normal, slow, fast speech speed
+    rint = random.randrange(0, 3)
+    threading.Thread(target=play_tagger_voice(rint)).start()
+    return seed[rint]
 
 def get_tagger_time() :
-    return 5
+    return 3
+
+def is_exist_id(id_list, id) :
+    flag = False
+    for i in id_list :
+        if i[0] == id and i[3] :
+            flag = True
+            break
+    return flag
+
+def draw_bbox(frame, color, bbox, id) :
+    x1 = int(bbox[0])
+    y1 = int(bbox[1])
+    x2 = int(bbox[2])
+    y2 = int(bbox[3])
+    m = 40 # margin
+    id_a = [0, 8, 20, 32]
+    cv2.rectangle(frame, (x1, y1), (x2, y2), color, 2)
+    cv2.rectangle(frame, (int((x1+x2)/2)-m, int((y1+y2)/2-m/2)), (int((x1+x2)/2)+m, int((y1+y2)/2+m/2)), color, -1)
+    cv2.putText(frame, str(id), (int((x1+x2)/2)-id_a[len(str(id))], int((y1+y2)/2)+8), 0, 1, (255, 255, 255), 2)
+    # cv2.rectangle(frame, (int(bbox[0]), int(bbox[1] - 30)), (int(bbox[0]) + 64, int(bbox[1])), color, -1)
+    # cv2.putText(frame, "id-" + str(track.track_id), (int(bbox[0]), int(bbox[1] - 10)), 0, 0.75, (255, 255, 255), 2)
 
 def person_track(vid, tracker, infer, encoder, surface) :
     frame_num = 0
@@ -154,9 +183,13 @@ def person_track(vid, tracker, infer, encoder, surface) :
     coord_1st = list()
     area_percent_list = list()
     turn = False # True : tagger see you
-    go_forward_time = get_go_forward_time()
+    go_forward_time = 5 #auth ids
+    # go_forward_time = get_go_forward_time()
     tagger_time = get_tagger_time()
-    sec_color = [0, 255, 0] # green
+    end_time = standard_time + 10 + go_forward_time
+    # sec_color = [255, 0, 0] # red
+    id_authing = True
+    id_authed = list() # [id(int), entry_time(int), dead_time(int), alive(bool)]
     while True:
         return_value, frame = vid.read()
         try :
@@ -253,14 +286,25 @@ def person_track(vid, tracker, infer, encoder, surface) :
                 sys.exit()
                 break
 
-        import person_track_id as pti
         # update tracks
         for track in tracker.tracks:
-            if not turn : continue
             if not track.is_confirmed() or track.time_since_update > 1:
                 continue
             bbox = track.to_tlbr()
-            class_name = track.get_class()
+            # class_name = track.get_class()
+            color = colors[int(track.track_id) % len(colors)]
+            color = [i * 255 for i in color]
+
+            if id_authing :
+                if not is_exist_id(id_authed, int(track.track_id)) :
+                    temp = [int(track.track_id), time.time(), 0, True]
+                    # print(temp)
+                    id_authed.append(temp)
+                draw_bbox(frame, color, bbox, track.track_id)
+                continue
+
+            if not turn: continue
+            if not is_exist_id(id_authed, int(track.track_id)) : continue
 
             if len(coord_1st) < 1:
                 coord_1st.append([int(track.track_id), int(bbox[0]), int(bbox[1]), int(bbox[2]), int(bbox[3])])
@@ -275,77 +319,127 @@ def person_track(vid, tracker, infer, encoder, surface) :
                 if item[0] == int(track.track_id) : now = item
 
             # draw bbox(now coordinate) on screen
-            color = colors[int(track.track_id) % len(colors)]
-            color = [i * 255 for i in color]
-            cv2.rectangle(frame, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), color, 2)
-            cv2.rectangle(frame, (int(bbox[0]), int(bbox[1] - 30)), (int(bbox[0]) + 64, int(bbox[1])), color, -1)
-            cv2.putText(frame, "id-" + str(track.track_id), (int(bbox[0]), int(bbox[1] - 10)), 0, 0.75, (255, 255, 255), 2)
+            draw_bbox(frame, color, bbox, track.track_id)
+            # cv2.rectangle(frame, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), color, 2)
+            # cv2.rectangle(frame, (int(bbox[0]), int(bbox[1] - 30)), (int(bbox[0]) + 64, int(bbox[1])), color, -1)
+            # cv2.putText(frame, "id-" + str(track.track_id), (int(bbox[0]), int(bbox[1] - 10)), 0, 0.75, (255, 255, 255), 2)
 
-            # cv2.rectangle(frame, (now[1], now[2]), (now[3], now[4]), color, 2)
-            # cv2.rectangle(frame, (now[1], now[2] - 30), (now[1] + 64, now[2]), color, -1)
-            # cv2.putText(frame, str(now[0]) + "-id", (now[1], now[2] - 10), 0, 0.75, (255, 255, 255), 2)
-
-            # red target circle
-            center = (int((bbox[0] + bbox[2]) / 2), int((bbox[1] + bbox[3]) / 2))
-            red = (255, 0, 0)
-            cv2.circle(frame, center, 20, red, 2)
-            cv2.circle(frame, center, 30, red, 2)
-            cv2.line(frame, (center[0] - 15, center[1]), (center[0] - 40, center[1]), red, 2)
-            cv2.line(frame, (center[0] + 15, center[1]), (center[0] + 40, center[1]), red, 2)
-            cv2.line(frame, (center[0], center[1] - 15), (center[0], center[1] - 40), red, 2)
-            cv2.line(frame, (center[0], center[1] + 15), (center[0], center[1] + 40), red, 2)
 
             # draw rectangle by first coordinate, calculate intersection area percentage
             if now :
-                cv2.rectangle(frame, (int(now[1]), int(now[2])), (int(now[3]), int(now[4])), color, 2)
-                cv2.rectangle(frame, (int(now[1]), int(now[2] - 30)), (int(now[1]) + 120, int(now[2])), color, -1)
-                cv2.putText(frame, str(track.track_id) + '-first', (int(now[1]), int(now[2] - 10)), 0, 0.75, (255, 255, 255), 2)
+                # first coordinate
+                # draw_bbox(frame, color, now[1:], track.track_id)
                 coord_now = bbox[:]
                 inter_area = 0
                 now_area = (coord_now[2] - coord_now[0]) * (coord_now[3] - coord_now[1])
                 inter = [max(now[1], coord_now[0]), max(now[2], coord_now[1]), min(now[3], coord_now[2]), min(now[4], coord_now[3])]
                 if inter[0] < inter[2] and inter[1] < inter[3] : inter_area = (inter[2] - inter[0]) * (inter[3] - inter[1])
-                area_percent_list.append((track.track_id, inter_area / now_area * 100))
+                rate = inter_area / now_area * 100
+                area_percent_list.append((track.track_id, rate))
 
-        # calculate frames per second of running detections
-        fps = 1.0 / (time.time() - start_time)
-        if frame_num % 10 == 0:
-            try:
-                # print('Frame # : {:>4} | FPS : {:>5.2f} | {:>6.2f}% | {:>6.2f}sec'
-                # .format(frame_num, fps, inter_area / now_area * 100, time.time() - standard_time))
-                print('Frame # : {:>4} | FPS : {:>5.2f}'.format(frame_num, fps))
-                for key, value in area_percent_list :
-                    print('id : {:>3} | area : {:>6.2f}%'.format(key, value))
-            except:
-                print("nothing detected or error occured")
+                # too move die
+                if rate < 75 :
+                    # red target circle
+                    center = (int((bbox[0] + bbox[2]) / 2), int((bbox[1] + bbox[3]) / 2))
+                    red = (255, 0, 0)
+                    cv2.circle(frame, center, 20, red, 2)
+                    cv2.circle(frame, center, 30, red, 2)
+                    cv2.line(frame, (center[0] - 15, center[1]), (center[0] - 40, center[1]), red, 2)
+                    cv2.line(frame, (center[0] + 15, center[1]), (center[0] + 40, center[1]), red, 2)
+                    cv2.line(frame, (center[0], center[1] - 15), (center[0], center[1] - 40), red, 2)
+                    cv2.line(frame, (center[0], center[1] + 15), (center[0], center[1] + 40), red, 2)
 
-        area_percent_list = list()
+                    # dead processing : voice, time added
+                    tagger_time += 2
+                    speak_str = str(track.track_id*2) + '번 탈락'
+                    gTTS(text=speak_str, lang='ko', slow=False).save('temp.mp3')
+                    pygame.mixer.music.load('temp.mp3')
+                    pygame.mixer.music.play()
+                    time.sleep(2)
 
-        result = np.asarray(frame)
-        result = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+                    for i in id_authed :
+                        if i[0] == track.track_id :
+                            i[2] = time.time() # write dead time
+                            i[3] = False
+                            print("id : {} | time : {:>6.2f} | % : {:>6.2f}".format(i[0], i[2]-i[1], rate))
 
-        surf = pygame.surfarray.make_surface(cv2.cvtColor((np.rot90(np.fliplr(result))), cv2.COLOR_BGR2RGB))
+                # time over die
+                if end_time < time.time() :
+                    for i in id_authed :
+                        if i[0] == track.track_id :
+                            i[3] = False
+                            print("id : {} | time : over".format(i[0]))
+
+        if not id_authing :
+            # calculate frames per second of running detections
+            fps = 1.0 / (time.time() - start_time)
+            if frame_num % 10 == 0:
+                try:
+                    # print('Frame # : {:>4} | FPS : {:>5.2f} | {:>6.2f}% | {:>6.2f}sec'
+                    # .format(frame_num, fps, inter_area / now_area * 100, time.time() - standard_time))
+                    print('Frame # : {:>4} | FPS : {:>5.2f}'.format(frame_num, fps))
+                    for key, value in area_percent_list :
+                        print('id : {:>3} | area : {:>6.2f}%'.format(key, value))
+                except:
+                    print("nothing detected or error occured")
+
+        # area_percent_list = list()
+
+        # result = np.asarray(frame)
+        # result = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+        # surf = pygame.surfarray.make_surface(cv2.cvtColor((np.rot90(np.fliplr(result))), cv2.COLOR_BGR2RGB))
+
+        alive = 0
+        for item in id_authed :
+            if item[-1] : alive += 1
+
+        surf = pygame.surfarray.make_surface((np.rot90(np.fliplr(frame))))
         if turn :
             surface.blit(pygame.transform.scale(surf, (1920, 1080)), (0, 0))
             if time.time() - standard_time > tagger_time : # tagger -> runner phase
                 go_forward_time = get_go_forward_time()
                 standard_time = time.time()
                 turn = False
-                sec_color = [0, 255, 0] # red -> green
+                # sec_color = [0, 255, 0] # red -> green
+                # print(id_authed)
         else :
-            surface.blit(pygame.transform.scale(pygame.image.load("pic4.png"), (1920, 1080)), [0, 0])
+            surface.blit(pygame.transform.scale(pygame.image.load("tagger.png"), (1920, 1080)), [0, 0])
+            if id_authing : surface.blit(pygame.transform.scale(surf, (1920, 1080)), (0, 0))
             if time.time() - standard_time > go_forward_time : # runner -> tagger phase
-                tagger_time = get_tagger_time()
-                standard_time = time.time()
-                turn = True
-                sec_color = [255, 0, 0] # green -> red
-                coord_1st = list()
+                if id_authing :
+                    id_authing = False
+                    go_forward_time = get_go_forward_time()
+                    standard_time = time.time()
+                    print(id_authed)
+                else :
+                    tagger_time = get_tagger_time()
+                    standard_time = time.time()
+                    turn = True
+                    # sec_color = [255, 0, 0] # green -> red
+                    coord_1st = list()
+                    # print(id_authed)
 
-        # for debug, timer show
-        secSurf = pygame.font.Font('freesansbold.ttf', 180).render('{:>5.2f}'.format(time.time() - standard_time), True, sec_color)
+        secSurf = pygame.font.Font('H2GTRE.TTF', 90).render(str(alive) + '생존', True, [0, 255, 0])
         secRect = secSurf.get_rect()
-        secRect.center = (180, 90)  # 1920 = width, 180 = font size
+        secRect.center = (1770, 45)  # 1920 = width, 180 = font size
         surface.blit(secSurf, secRect)
+
+        # timer
+        if not id_authing :
+            # for debug, timer show
+            display_time = end_time - time.time()
+            if display_time < 0 : display_time = 0
+            secSurf = pygame.font.Font('freesansbold.ttf', 180).render('{:>3.0f}'.format(display_time), True, [255, 0, 0])
+            secRect = secSurf.get_rect()
+            secRect.center = (120, 90)  # 1920 = width, 180 = font size
+            surface.blit(secSurf, secRect)
+
+            # game end : no alive or time over
+            if alive == 0 or end_time + 2 < time.time() :
+                pygame.mixer.music.load('end_voice.mp3')
+                pygame.mixer.music.play()
+                time.sleep(2)
+                return id_authed
 
         pygame.display.flip()
 
@@ -354,7 +448,7 @@ def person_track(vid, tracker, infer, encoder, surface) :
 
 def main(_argv):
     # opening phase
-    opening_video()
+    # opening_video()
 
     # intro phase
     intro_screen()
@@ -386,12 +480,23 @@ def main(_argv):
     vid.set(cv2.CAP_PROP_FRAME_WIDTH, 1280) # will get from webcam max constant?
     vid.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
+    opening_video()
+
     pygame.display.set_caption("main game playing")
     surface = pygame.display.set_mode((1920, 1080)) # screen succeed?
-    surface.blit(pygame.transform.scale(pygame.image.load("pic4.png"), (1920, 1080)), [0, 0])
+    surface.blit(pygame.transform.scale(pygame.image.load("tagger.png"), (1920, 1080)), [0, 0])
     pygame.display.update()
 
-    person_track(vid, tracker, infer, encoder, surface) # while video is running
+    result = person_track(vid, tracker, infer, encoder, surface) # while video is running
+    print(result)
+
+    # result phase
+    pygame.display.set_caption("result")
+    surface = pygame.display.set_mode((1920, 1080))  # screen succeed?
+    surface.blit(pygame.transform.scale(pygame.image.load("result.png"), (1920, 1080)), [0, 0])
+    pygame.display.update()
+    time.sleep(3)
+
     pygame.quit()
     cv2.destroyAllWindows()
 
